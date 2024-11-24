818bb49ac8283a686c74759a1dc22c0de16a9973
Ladizslav <kidalsai@gmail.com>
2024-11-24 20:51:55 +0100

    Seventh commit

--- public/script.js
+++ public/script.js
@@ -15,49 +15,9 @@ socket.on('disconnect', () => {
     connectionStatus.style.color = 'red';
 });
 
-function saveCaretPosition(editableDiv) {
-    const selection = window.getSelection();
-    const range = selection.getRangeAt(0);
-    const preCaretRange = range.cloneRange();
-    preCaretRange.selectNodeContents(editableDiv);
-    preCaretRange.setEnd(range.endContainer, range.endOffset);
-    return preCaretRange.toString().length; 
-}
-
-function restoreCaretPosition(editableDiv, position) {
-    const selection = window.getSelection();
-    const range = document.createRange();
-    let currentPos = 0;
-
-    function traverseNodes(node) {
-        if (node.nodeType === Node.TEXT_NODE) {
-            const textLength = node.textContent.length;
-            if (currentPos + textLength >= position) {
-                range.setStart(node, position - currentPos);
-                range.collapse(true);
-                return true;
-            }
-            currentPos += textLength;
-        } else {
-            for (let i = 0; i < node.childNodes.length; i++) {
-                if (traverseNodes(node.childNodes[i])) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    traverseNodes(editableDiv);
-    selection.removeAllRanges();
-    selection.addRange(range);
-}
-
 socket.on('document', (content) => {
     if (!isUpdating) {
-        const caretPosition = saveCaretPosition(editor); 
         editor.innerText = content; 
-        restoreCaretPosition(editor, caretPosition); 
     }
 });
 
@@ -65,24 +25,4 @@ socket.on('clients-update', (clients) => {
     userList.innerHTML = '';
     clients.forEach(client => {
         const li = document.createElement('li');
-        li.textContent = client.name;
-        userList.appendChild(li);
-    });
-});
-
-socket.on('patch', (patches) => {
-    const diffMatchPatch = new diff_match_patch();
-    const result = diffMatchPatch.patch_apply(patches, editor.innerText);
-    editor.innerText = result[0];
-});
-
-editor.addEventListener('input', () => {
-    isUpdating = true; 
-    const content = editor.innerText;
-    socket.emit('edit', { content });
-
-    setTimeout(() => {
-        isUpdating = false;
-    }, 500);
-});
-
+        li.textContent =
