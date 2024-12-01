a828a3596eaa72375bcb78a80be1f3c3e74a4c32
Ladizslav <kidalsai@gmail.com>
2024-12-01 16:04:48 +0100

    Nineteenth commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket("ws://<public-ip>:8080");
+const ws = new WebSocket("ws://localhost:8080");
 const editor = document.getElementById('editor');
 const editorContainer = document.getElementById('editor-container');
 const connectionStatus = document.getElementById('connection-status');
